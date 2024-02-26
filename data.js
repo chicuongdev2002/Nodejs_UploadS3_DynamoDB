cb253289e511b9ae3a929f262c572c39cad0f33c
H82M19\Student <Student>
2024-02-26 13:42:19 +0700

    create with nodejs

---
+++ data.js
@@ -0,0 +1,24 @@
+const courses=[
+    {
+    id:1,
+    name:"Co so du lieu",
+    course_type:"Co so",
+    semester:'HK1-2020-2021',
+    department:'K.CNTT'
+    },
+    {
+        id:2,
+        name:"Cau truc du lieu",
+        course_type:"Co so",
+        semester:'HK1-2020-2021',
+        department:'K.CNTT'
+        },
+        {
+            id:3,
+            name:"Cong nghe phan mem",
+            course_type:"Co so",
+            semester:'HK1-2020-2021',
+            department:'K.CNTT'
+            }
+]
+module.exports=courses;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express=require('express');
+const courses=require('./data');
 const app=express();
+//register middlewares
+app.use(express.json({extended:false}));
+app.use(express.static('./templates'));
+//config view
+app.set('view engine','ejs');
+app.set('views','./templates');
+//router
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    return res.render('index',{data:courses});
+});
+app.post('/',(req,res)=>{
+   console.log(req.body);
 });
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
