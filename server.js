23beaedda8326c2df99cd720dd8db29aed9e6d66
Nguyen Chi Cuong <20020331.cuong@student.iuh.edu.vn>
2024-03-04 12:33:44 +0700

    connect dynamodb with aws

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const coursesData = require('./data');
 const app = express();
 const upload = multer();
+// Cấu hình aws DynamoDB
+const AWS = require('aws-sdk');
+const config = new AWS.Config({
+    accessKeyId: 'AKIA6GBMFE2DOCWHYX4N',
+    secretAccessKey:'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',
+    region:'ap-southeast-2'
+});
+AWS.config = config;
 
-// Biến toàn cục để lưu trữ ID hiện tại
-let currentId = coursesData.length+1;
-let courses = [...coursesData];
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName ='MonHoc';
 //register middlewares
 app.use(express.json({ extended: false }));
 app.use(express.static('./templates'));
@@ -15,38 +21,69 @@ app.use(express.static('./templates'));
 app.set('view engine', 'ejs');
 app.set('views', './templates');
 
-//router
+//Get
 app.get('/', (req, res) => {
-    return res.render('index', { data: courses });
-});
-
-app.post('/', upload.single('image'), (req, res) => {
-    const newCourse = {
-        id: currentId++,
-        name: req.body.name,
-        course_type: req.body.course_type,
-        semester: req.body.semester,
-        department: req.body.department,
-        image: req.file ? req.file.filename : null
+    const params ={
+        TableName: tableName,
     };
+    docClient.scan(params, (err, data) => {
+        if(err){
+            res.send('Internal Server Error');
+        }else{
+            return res.render('index', {data: data.Items});
+        }
+    });
 
-    courses.push(newCourse);
-    return res.redirect('/');
 });
-
+//add
+app.post('/', upload.fields([]), (req, res) => {
+    const{id,name, course_type,semester,department} = req.body;
+    const params = {
+        TableName: tableName,
+        Item: {
+            "id":id,
+    'name':name,
+    'course_type':course_type,
+        'semester':semester,
+        'department':department
+        }
+    }
+    docClient.put(params, (err, data) => {
+        if(err){
+            return res.send('Internal Server Error')
+        }else{
+            return res.redirect("/");
+        }
+    })
+});
 //delete
 app.post('/delete', upload.fields([]), (req, res) => {
-    //Lấy list bao gồm các ô checkbox
     const listItems = Object.keys(req.body);
-    console.log(listItems);
-    //Nếu không chọn vào check box trả về trang ban đầu
     if(listItems.length === 0){
         return res.redirect("/")
     }
-    courses = courses.filter(item => !listItems.includes(item.id.toString()));
-    return res.redirect("/");
+    function onDeleteItem(index){
+        const params = {
+            TableName: tableName,
+            Key:{
+                "id": listItems[index]
+            }
+        }
+    docClient.delete(params, (err, data) => {
+        if(err){
+            return res.send('Internal Server Error')
+        }else{
+            if(index > 0){
+                onDeleteItem(index - 1);
+            }else{
+                return res.redirect("/");
+            }
+            
+        }
+    })
+}
+    onDeleteItem(listItems.length - 1);
 });
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
